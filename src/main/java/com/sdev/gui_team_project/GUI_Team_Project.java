d1eeb80817d7ff662c7d40f0b10ec66b01b4e355
Brian Twene <twene521@gmail.com>
2022-11-04 23:13:19 +0000

    Inital Commit: Created project

---
+++ src/main/java/com/sdev/gui_team_project/GUI_Team_Project.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sdev.gui_team_project;
+
+/**
+ *
+ * @author twene
+ */
+public class GUI_Team_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is the main file for our GUI project");
+    }
+}
